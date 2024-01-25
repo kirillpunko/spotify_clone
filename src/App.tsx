ef67a9195493ee0f5565555d1bbd16b3b770a782
Killerfrizz <kirill22561@gmail.com>
2024-01-25 13:00:33 +0300

    add some styles for main window

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Right from "./components/right";
 import Left from "./components/left";
+import "./index.scss"
 
 function App() {
-  return <div>
+  return <div className={"mainWin"}>
       <Left/>
       <Right/>
   </div>;
